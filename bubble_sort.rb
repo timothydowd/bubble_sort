0cef20e5145387e5f16235eae9abc3b20fb01ac2
dwdy250 <timdowd1984@gmail.com>
2018-08-05 13:17:57 +0100

    +

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,16 +7,16 @@
 def bubble_sort(array)
   array.map do |e|
     if e > array[array.index(e)+1] # if e is bigger than next element
-      index_a = array[array.index(e)]
-      index_b = array[array.index(e)+1]
+      index_l = array[array.index(e)]
+      index_r = array[array.index(e)+1]
 
-      array[index_a], array[index_b] = array[index_b], array[index_a]
+      array[index_l], array[index_r] = array[index_r], array[index_l]
       #array[array.index(e)], array[array.index(e)+1] = array[array.index(e)+1], array[array.index(e)]
 
     end
-    #puts "#{e} , #{array[array.index(e)+1]}"
+    puts "#{e} , #{array[array.index(e)+1]}"
   end
-  puts array
+
 
 
 
